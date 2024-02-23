529e3acdc6087234e653f9d3b5603814f6915d4d
jonathan <jonathan.ewers.2022@uni.strath.ac.uk>
2024-02-23 18:34:59 +0000

    new controller to handle Auth

---
+++ src/main/java/uk/co/asepstrath/bank/bank/AuthController.java
@@ -0,0 +1,77 @@
+package uk.co.asepstrath.bank.bank;
+
+import io.jooby.ModelAndView;
+import io.jooby.StatusCode;
+import io.jooby.annotation.GET;
+import io.jooby.annotation.POST;
+import io.jooby.annotation.Path;
+import io.jooby.annotation.QueryParam;
+import io.jooby.exception.StatusCodeException;
+import org.slf4j.Logger;
+import uk.co.asepstrath.bank.example.MyMessage;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+    Example Controller is a Controller from the MVC paradigm.
+    The @Path Annotation will tell Jooby what /path this Controller can respond to,
+    in this case the controller will respond to requests from <host>/example
+ */
+@Path("/bank/Login")
+public class AuthController {
+
+    /*
+    This constructor can take in any dependencies the controller may need to respond to a request
+     */
+    private BankController bankController;
+    public AuthController(BankController controller) {
+        bankController = controller;
+    }
+    @POST
+    public ModelAndView AuthenticateLogin(String ID, String password) {
+        if(ID == null || password == null){
+            return bankController.login();
+        }
+        try(Connection connection = bankController.getDataSource().getConnection()){
+
+            // Use a prepared statement to avoid SQL injection vulnerabilities
+            PreparedStatement statementAccounts = connection.prepareStatement("SELECT * FROM `accountsTable` WHERE `id` = ?");
+            // Set the accountID parameter in the prepared statement
+            statementAccounts.setString(1, ID);
+            ResultSet setAccounts = statementAccounts.executeQuery();
+
+            //execute query for stored passwords
+            PreparedStatement statementPassword = connection.prepareStatement("SELECT * FROM `accountsPassword` WHERE `id` = ?");
+            statementPassword.setString(1,ID);
+            ResultSet setPassword = statementPassword.executeQuery();
+            while(setPassword.next()){
+                while(setAccounts.next()) {
+
+                    if (setAccounts.getString("id").equals(setPassword.getString("id")) && setPassword.getString("password").equals(password)  ) {
+                        return bankController.viewAccounts(ID);
+                    }
+                }
+
+            }
+
+            setAccounts.close();
+            setPassword.close();
+            return bankController.login();
+
+
+
+        } catch (SQLException e) {
+            // If something does go wrong this will log the stack trace
+            bankController.getLogger().error("Database Error Occurred", e);
+            // And return a HTTP 500 error to the requester
+            throw new StatusCodeException(StatusCode.SERVER_ERROR, "Database Error Occurred");
+        }
+
+    }
+
+
+
+}
