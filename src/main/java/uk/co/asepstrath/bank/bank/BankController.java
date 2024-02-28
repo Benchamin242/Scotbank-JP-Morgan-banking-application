e8e7505a9cd882453035eba8769132c869582a9e
jonathan <jonathan.ewers.2022@uni.strath.ac.uk>
2024-02-28 16:51:11 +0000

    Reapply "ammended the login section on BankController"
    This reverts commit 119b66e49dd404c15db8de5b42b6b4a97b450939.

--- src/main/java/uk/co/asepstrath/bank/bank/BankController.java
+++ src/main/java/uk/co/asepstrath/bank/bank/BankController.java
@@ -5,6 +5,7 @@ import io.jooby.StatusCode;
 import io.jooby.annotation.*;
 import io.jooby.exception.StatusCodeException;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import kong.unirest.core.Unirest;
 import org.slf4j.Logger;
 import uk.co.asepstrath.bank.Account;
@@ -26,6 +27,7 @@ import java.util.Random;
     The @Path Annotation will tell Jooby what /path this Controller can respond to,
     in this case the controller will respond to requests from <host>/example
  */
+@Singleton
 @Path("/bank")
 public class BankController {
 
@@ -45,6 +47,12 @@ public class BankController {
     The @GET annotation denotes that this function should be invoked when a GET HTTP request is sent to <host>/example
     The returned string will then be sent to the requester
      */
+    public DataSource getDataSource(){
+        return dataSource;
+    }
+    public Logger getLogger(){
+        return logger;
+    }
     @GET
     public String welcome() {
 
@@ -107,7 +115,7 @@ public class BankController {
     }
     @POST
     @Path("/viewAccount")
-    public ModelAndView submit(String accountID) {
+    public ModelAndView submit(String accountID, String password) {
         if(accountID == null){
             accountID = "1";
         }
@@ -126,6 +134,7 @@ public class BankController {
                 model.put("id", set.getInt("id"));
 
             }
+
             set.close();
             return new ModelAndView("simpleDetails.hbs", model);
 
