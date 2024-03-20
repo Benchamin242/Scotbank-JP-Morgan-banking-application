327610852177febff29417be827d417610e0f14c
jonathan <jonathan.ewers.2022@uni.strath.ac.uk>
2024-03-20 14:55:21 +0000

    more tests

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 import io.jooby.test.MockRouter;
 import org.junit.jupiter.api.Test;
+import uk.co.asepstrath.bank.bank.AuthController;
 import uk.co.asepstrath.bank.bank.BankController;
 import io.jooby.ModelAndView;
 import org.junit.jupiter.api.Test;
@@ -235,6 +236,26 @@ public class UnitTest {
 
         assertEquals(new BigDecimal("10.00"), to.getStartingBalance());
         assertEquals(new BigDecimal("5.00"), from.getStartingBalance());
+    }
+
+    @Test
+    public void viewAllTransactions() throws SQLException {
+        App app = new App();
+        app.onStart();
+        AuthController authController = app.authController;
+        BankController bankController = app.bankController;
+        MockContext context = new MockContext();
+        Session session = context.session();
+        session.put("id","635e583f-0af2-47cb-9625-5b66ba30e188");
+
+        ModelAndView model = bankController.viewAllTransactions(session, context);
+
+
+        assertFalse(model.getView().isBlank());
+        Map<String, Object> map = model.getModel();
+        @SuppressWarnings("unchecked")
+        ArrayList<Transactions> transaction = (ArrayList<Transactions>) map.get("transactions");
+        assertTrue(transaction != null); //tests whether there are transactions
     }
 
 
